88b1ba4785a43972b1acc7744226e43e3603bc13
Siddharth-Shenoy <siddh.shenoy@gmail.com>
2018-03-21 08:41:16 +0530

    Add files via upload

--- stringextra.h
+++ stringextra.h
@@ -1,9 +1,39 @@
 
 #ifndef __STRINGEXTRA_H_INCLUDED__
 #define __STRINGEXTRA_H_INCLUDED__
+
 namespace extra
 {
+    typedef struct dictionary
+    {
+        std::string _entry;
+        int count;
+    }
+    dictionary;
+    
+    int occurance(const std::string& string,const std::string& finddata);
     std::string *splitstr(const std::string& string,int *total,char delimiter = ' ');
+
+
+    int occurance(const std::string& string,const std::string& finddata)
+    {
+        int _i;
+        int _total = 0;
+        for(_i = 0; _i < string.length(); _i++)
+        {
+            if(string[_i] == finddata[0])
+            {
+                if(_i + finddata.length() <= string.length())
+                {
+                    if(string.substr(_i,finddata.length()) == finddata)
+                        _total++;
+                }
+                else break;
+            }
+            else continue;
+        }
+        return _total;
+    }
     std::string *splitstr(const std::string& string,int *total,char delimiter)
     {
         int _i,_j;
@@ -33,4 +63,5 @@ namespace extra
         return retsplit;
     }
 };
+
 #endif
